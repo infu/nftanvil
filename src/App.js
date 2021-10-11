d183fe167c2938f76405be3a85fcf42230e153c4
infu <vtsukev@aula.bg>
2021-10-11 22:31:44 +0300

    making react work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,54 @@
 import logo from './logo.svg';
 import './App.css';
+import { AuthClient } from "@dfinity/auth-client";
+import { dropship, createActor } from "./canisters/dropship";
+import {principalToAccountIdentifier, encodeTokenId} from "./purefunc/token";
+
+
+
+function onAuthenticate() {
+
+     let authClient = await new Promise(async (resolve, reject) => {
+
+        let authClient = await AuthClient.create();
+        authClient.login({
+          identityProvider: "http://localhost:8000?canisterId=rkp4c-7iaaa-aaaaa-aaaca-cai",
+          onSuccess: async (e) => {
+            // authClient now has an identity
+            console.log("Success")
+
+            resolve(authClient)
+          },
+          onError: reject
+        })
+    });
+
+
+    const identity = await authClient.getIdentity();
+    let principal = identity.getPrincipal().toString();
+    console.log("principal: ", principal);
+    let accountId = principalToAccountIdentifier(principal);
+    console.log("accountId: ", accountId);
+
+    let owned = await dropship.owned({"address": accountId});
+    console.log("Owned: ", owned)
+
+    console.log("DROPSHIP CANISTER ID "+process.env.REACT_APP_DROPSHIP_CANISTER_ID);
+
+    let dropshipCid = process.env.DROPSHIP_CANISTER_ID;
+
+    let a1 = await dropship.whoAmI();
+    let actor = createActor(process.env.DROPSHIP_CANISTER_ID, {agentOptions:{identity}});
+    let a2 = await actor.whoAmI();
+    console.log("a1",a1.toString());
+    console.log("a2",a2.toString());
+
+    let tokenId = encodeTokenId(dropshipCid,15);
+    console.log("Token id: "+tokenId)
+}
+
+
+
 
 function App() {
   return (
